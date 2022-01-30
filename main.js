fbc2954c79006d8327904abdb26c89d521ff2e1e
wallyson ruan <wallysonruan@gmail.com>
2022-01-30 19:07:09 -0300

    Now the cards are double styled

--- main.js
+++ main.js
@@ -4,21 +4,26 @@ const toggler = document.getElementById("theme-change");
 const body = document.getElementsByTagName("body")[0],
     firstTitle = document.getElementById("first-title");
     secondTitle = document.getElementById("second-title"),
-    cards = document.getElementsByClassName("card");
+    cards = document.querySelectorAll(".card");
 
 
-toggler.addEventListener("aloha", function(){
+toggler.addEventListener("input", function(){
     if (toggler.checked == true){
         body.classList.add("body-light");
         firstTitle.classList.add("first-title--light");
         secondTitle.classList.add("second-title--light");
+
+        for(let i = 0; i < cards.length; i++){
+            cards[i].classList.add("card--light");
+        }
     }
     else{
         body.classList.remove("body-light");
         firstTitle.classList.remove("first-title--light");
         secondTitle.classList.remove("second-title--light");
 
+        for(let i = 0; i < cards.length; i++){
+            cards[i].classList.remove("card--light");
+        }
     }
 })
-
-console.log(cards[0].classList.add("card--light"))
